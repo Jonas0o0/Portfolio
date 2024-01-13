1d134b4df25938510b3ee2b88ff80138e3d692f3
Jonas <83239419+Jonas0o0@users.noreply.github.com>
2024-01-13 19:13:37 +0100

    Add files via upload

--- Js/index.js
+++ Js/index.js
@@ -90,3 +90,20 @@ class Parallax2 {
 
 Parallax.blind();
 Parallax2.blind();
+
+var burger = document.getElementsByClassName('burguer')[0];
+var bar1 = document.getElementsByClassName('up')[0];
+var bar2 = document.getElementsByClassName('mid')[0];
+var bar3 = document.getElementsByClassName('down')[0];
+var link = document.getElementsByClassName('link')[0];
+var mainContent = document.getElementsByClassName('main-content')[0];
+
+burger.addEventListener('click', swap);
+
+function swap(){
+    bar1.classList.toggle('active');
+    bar2.classList.toggle('active');
+    bar3.classList.toggle('active');
+    link.classList.toggle('active');
+    mainContent.classList.toggle('active');
+}
